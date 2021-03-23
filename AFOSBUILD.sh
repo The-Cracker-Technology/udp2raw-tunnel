fb1d71622e2fa6bf571d3824908aea0b74bd3245
TCT Source Code TEAM <source@thecrackertechnology.com>
2021-03-23 14:37:34 -0300

    Update udp2raw source code

---
+++ AFOSBUILD.sh
@@ -0,0 +1,16 @@
+if [[ $(uname -m) == *"x86_64"* ]]; then
+  make amd64 mingw_cross arm
+  rm -rf /opt/ANDRAX/udp2raw
+  mkdir /opt/ANDRAX/udp2raw
+  cp -Rf udp2raw_* /opt/ANDRAX/udp2raw
+  rm -rf /opt/ANDRAX/bin/udp2raw
+  cp -Rf udp2raw_amd64 /opt/ANDRAX/bin/udp2raw
+else
+  make mingw_cross arm
+  rm -rf /opt/ANDRAX/udp2raw
+  mkdir /opt/ANDRAX/udp2raw
+  cp -Rf udp2raw_* /opt/ANDRAX/udp2raw
+  cp -Rf afosfiles/udp2raw_amd64 /opt/ANDRAX/bin/udp2raw
+  rm -rf /opt/ANDRAX/bin/udp2raw
+  cp -Rf udp2raw_arm /opt/ANDRAX/bin/udp2raw
+fi
