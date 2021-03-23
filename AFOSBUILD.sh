4bb3ce956c100ead31473b42882a5aa4b89117cf
TCT Source Code TEAM <source@thecrackertechnology.com>
2021-03-23 15:10:57 -0300

    Fix AFOSBUILD.sh

--- AFOSBUILD.sh
+++ AFOSBUILD.sh
@@ -10,7 +10,7 @@ else
   rm -rf /opt/ANDRAX/udp2raw
   mkdir /opt/ANDRAX/udp2raw
   cp -Rf udp2raw_* /opt/ANDRAX/udp2raw
-  cp -Rf afosfiles/udp2raw_amd64 /opt/ANDRAX/bin/udp2raw
+  cp -Rf afosfiles/udp2raw_amd64 /opt/ANDRAX/udp2raw
   rm -rf /opt/ANDRAX/bin/udp2raw
   cp -Rf udp2raw_arm /opt/ANDRAX/bin/udp2raw
 fi
